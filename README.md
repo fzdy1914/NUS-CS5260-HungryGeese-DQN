dc262ac7c55220c91509b336e5cf798a8eb5b67f
WANG CHAO <1229983126@qq.com>
2021-02-02 00:27:56 +0800

    Add some test file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Requirements
+
+1. `python 3.8.5`
+1. `pytorch`
+1. `jupyterlab`
+1. `pip install kaggle_environments cpprb requests`
+
---
+++ test_env.py
@@ -0,0 +1,7 @@
+from kaggle_environments import make
+from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Configuration, Action, row_col
+
+test_env = make("hungry_geese", debug=True)
+
+test_env.run(["test_agent.py", "test_agent.py"])
+test_env.render()
